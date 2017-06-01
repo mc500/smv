0e2456903afec44ad8b6e6855d6edf2f5b2e631f
HONGJS <hongjs@kr.ibm.com>
2017-06-01 18:56:07 +0900

    Revert "applied aslant"
    This reverts commit 73c7d1b78d3d8db9e14a8b9ac6f2c4bd7663146e.

--- projects/smv-userauth/test/test-authtokenhelper.js
+++ projects/smv-userauth/test/test-authtokenhelper.js
@@ -1,82 +1,81 @@
 #!/usr/local/bin/node
 
-/* eslint-env mocha */
-
 const TEST_TIMEOUT = 60*1000; // 60 seconds
 
 var assert = require('assert'),
-  SMVAuthTokenHelper = require('../controllers/SMVAuthTokenHelper');
+    SMVAuthTokenHelper = require('../controllers/SMVAuthTokenHelper');
 
 describe('[SMVAuthTokenHelper Unit Test]', function() {
-  var authtoken;
 
-  before(function () {
-    //console.log('>>>>>>>>>>>>>>> before');
-  });
+    var authtoken;
 
-  after(function () {
-    //console.log('after <<<<<<<<<<<<<<<<');
-  });
-
-  this.timeout(TEST_TIMEOUT);
-
-  it('AuthToken generation', function (done) {
-    SMVAuthTokenHelper.generateAuthToken(function(token) {
-      assert(token);
-
-      // keep this. it will be used later testcases
-      authtoken = token;
-
-      done();
+    before(function () {
+        //console.log('>>>>>>>>>>>>>>> before');
     });
-  });
 
-  it('AuthToken validation', function (done) {
-    assert(authtoken);
-    SMVAuthTokenHelper.isValidAuthToken(authtoken, function(valid) {
-      assert(valid);
-
-      done();
+    after(function () {
+        //console.log('after <<<<<<<<<<<<<<<<');
     });
-  });
 
-  it('AuthToken validation negative test', function (done) {
-    SMVAuthTokenHelper.isValidAuthToken('mykey', function(valid) {
-      assert(!valid);
+    this.timeout(TEST_TIMEOUT);
 
-      done();
+    it('AuthToken generation', function (done) {
+        SMVAuthTokenHelper.generateAuthToken(function(token) {
+            assert(token);
+
+            // keep this. it will be used later testcases
+            authtoken = token;
+
+            done();
+        });
     });
-  });
 
-  it('AuthToken set value', function (done) {
-    assert(authtoken);
-    SMVAuthTokenHelper.setAuthTokenValue(authtoken, 'myfield', 'myvalue', function(result) {
-      assert(result);
+    it('AuthToken validation', function (done) {
+        assert(authtoken);
+        SMVAuthTokenHelper.isValidAuthToken(authtoken, function(valid) {
+            assert(valid);
 
-      done();
+            done();
+        });
     });
-  });
 
-  it('AuthToken get value', function (done) {
-    assert(authtoken);
-    SMVAuthTokenHelper.getAuthTokenValue(authtoken, 'myfield', function(result) {
-      assert(result == 'myvalue');
+    it('AuthToken validation negative test', function (done) {
+        SMVAuthTokenHelper.isValidAuthToken('mykey', function(valid) {
+            assert(!valid);
 
-      done();
+            done();
+        });
     });
-  });
 
-  it('AuthToken invalidate', function (done) {
-    assert(authtoken);
-    SMVAuthTokenHelper.invalidateAuthToken(authtoken, function(valid) {
-      assert(valid);
+    it('AuthToken set value', function (done) {
+        assert(authtoken);
+        SMVAuthTokenHelper.setAuthTokenValue(authtoken, 'myfield', 'myvalue', function(result) {
+            assert(result);
 
-      SMVAuthTokenHelper.isValidAuthToken(authtoken, function(valid) {
-        authtoken = undefined;
-        assert(!valid);
-
-        done();
-      });
+            done();
+        });
     });
-  });
+
+    it('AuthToken get value', function (done) {
+        assert(authtoken);
+        SMVAuthTokenHelper.getAuthTokenValue(authtoken, 'myfield', function(result) {
+            assert(result == 'myvalue');
+
+            done();
+        });
+    });
+
+    it('AuthToken invalidate', function (done) {
+        assert(authtoken);
+        SMVAuthTokenHelper.invalidateAuthToken(authtoken, function(valid) {
+            assert(valid);
+
+            SMVAuthTokenHelper.isValidAuthToken(authtoken, function(valid) {
+                authtoken = undefined;
+                assert(!valid);
+
+                done();
+            });
+        });
+    });
 });
