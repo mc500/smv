637efb9540fccdafcd66b9719c66842984b192c6
HONGJS <hongjs@kr.ibm.com>
2017-06-20 16:26:20 +0900

    smv-ui-app initial

---
+++ projects/smv-ui-app/smv-ui-app.sh
@@ -0,0 +1,6 @@
+export PORT=3000
+export SMV_BADGE_BASE_URL=http://localhost:6001
+export SMV_USERAUTH_BASE_URL=http://localhost:6002
+export SMV_USERINFO_BASE_URL=http://localhost:6003
+export SMV_VISIT_BASE_URL=http://localhost:6004
+npm start
